aee469232d97625266c6fce3cdbb5e8abda82b18
Dinis Dzhula <aluno27657@ipt.pt>
2025-07-03 11:12:52 +0100

    json lido corretamente

--- prog_web/frontend/src/app/teste/page.js
+++ prog_web/frontend/src/app/teste/page.js
@@ -1,13 +1,20 @@
 export default async function Home() {
   const res = await fetch('http://localhost:1337/api/eventos', { cache: 'no-store' });
-  const data = await res.json();
-  console.log(data);
+  const json = await res.json();
+  const eventos = json.data;
 
   return (
     <div>
       <h1>Eventos</h1>
       <ul>
-        <pre>{JSON.stringify(data, null, 2)}</pre>
+        {eventos.map((evento) => (
+          <li key={evento.id}>
+            <strong>Nome:</strong> {evento.nome} <br />
+            <strong>Descrição:</strong> {evento.descricao} <br />
+            <strong>Data:</strong> {new Date(evento.data).toLocaleDateString()} <br />
+            <strong>Status:</strong> {evento.statu_s}
+          </li>
+        ))}
       </ul>
     </div>
   );
